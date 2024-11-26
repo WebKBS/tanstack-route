c9a467bc7a0fe56a65cad0acdfaf02b6be7af0d8
kbs21c1987 <kbs21c1987@gmail.com>
2024-11-26 20:32:27 +0900

    init

---
+++ tanstack-route-dynamic/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import { Link } from '@tanstack/react-router';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Tanstack Route</h1>
+      <div className="navigation">
+        <Link to="/">Home</Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
